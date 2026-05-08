25a6c266de30ada1f1a2a673864a0c08b34de186
Abdul basith <basithofficialmail01@gmail.com>
2026-05-08 15:38:16 +0530

    fix: user not found error handling

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,10 +2,10 @@ import { pool } from '../db/db.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 async function registerUser(req, res) {
-    const { name, email, password, created_at } = req.body
+    const { name, email, password } = req.body
     const password_hash = await bcrypt.hash(password, 10)
     try {
-        const result = await pool.query('INSERT INTO users (name,email,password_hash,created_at ) VALUES($1,$2,$3,$4)', [name, email, password_hash, created_at])
+        const result = await pool.query('INSERT INTO users (name,email,password_hash) VALUES($1,$2,$3)', [name, email, password_hash])
         res.json({ message: "user created successfully" })
     } catch (err) {
         console.error('DB ERROR', err)
@@ -14,9 +14,13 @@ async function registerUser(req, res) {
     }
 }
 async function loginUser(req, res) {
+    console.log("Login request received with body:", req.body);
     const { email, password } = req.body
     try {
         const result = await pool.query('SELECT * from users where email=$1', [email])
+        if(result.rows.length === 0) {
+            return res.status(400).json({ error: "user not found" })
+        }
         const isMatch = await bcrypt.compare(password, result.rows[0].password_hash)
         if (isMatch) {
             const token = jwt.sign({ userId: result.rows[0].id }, process.env.JWT_SECRET, { expiresIn: '5d' })
