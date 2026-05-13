8893bcc2463af62c9d612e349e66ea639a9562fc
Abdul basith <basithofficialmail01@gmail.com>
2026-05-13 13:35:47 +0530

    feat: implement ProtectedRoute and Layout components for enhanced routing and authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,20 @@ import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import GroceryPurchases from "./pages/GroceryPurchases";
 import GeneralExpenses from "./pages/GeneralExpenses";
+import { Layout } from "./components/Layout";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
       <Route path="/auth" element={<Auth />} />
-      <Route path="/grocery" element={<GroceryPurchases />} />
-      <Route path="/general" element={<GeneralExpenses />} />
+      <Route path='/' element={<ProtectedRoute><Layout /></ProtectedRoute>} >
+        <Route path="/" element={<Dashboard />} />
+
+        <Route path="/grocery" element={<GroceryPurchases />} />
+        <Route path="/general" element={<GeneralExpenses />} />
+      </Route>
+
     </Routes>
   );
 }
